8c8fdbcf3e783c49cbe5cf4175a4bfd3d91758dd
Luca Savio <savioluca2@gmail.com>
2026-06-01 21:31:02 +0200

    fixed

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,10 +2,23 @@ import Database from 'better-sqlite3'
 import { drizzle } from 'drizzle-orm/better-sqlite3'
 import * as schema from './schema'
 
-const dbPath = process.env.DATABASE_URL ?? './data/investment.db'
+type Db = ReturnType<typeof drizzle<typeof schema>>
 
-const sqlite = new Database(dbPath)
-sqlite.pragma('journal_mode = WAL')
-sqlite.pragma('foreign_keys = ON')
+function initDb(): Db {
+  const dbPath = process.env.DATABASE_URL ?? './data/investment.db'
+  const sqlite = new Database(dbPath)
+  sqlite.pragma('journal_mode = WAL')
+  sqlite.pragma('foreign_keys = ON')
+  return drizzle(sqlite, { schema })
+}
 
-export const db = drizzle(sqlite, { schema })
+let _db: Db | null = null
+
+// Proxy defers the actual DB connection until the first method call,
+// so importing this module at build time doesn't open the file.
+export const db = new Proxy({} as Db, {
+  get(_, prop) {
+    if (!_db) _db = initDb()
+    return Reflect.get(_db, prop)
+  },
+})
